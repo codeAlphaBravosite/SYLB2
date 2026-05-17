fc08ad131df0ed648f0c76b2bc8995971463269b
codeAlphaBravosite <help.factchannel@gmail.com>
2026-05-18 02:47:27 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Fixed sw.js — Fix #30: cache name MUST change with each deploy */
-const CACHE_NAME = "syllabus-tracker-v7";
+const CACHE_NAME = "syllabus-tracker-v8";
 
 const PRECACHE = [
   "./",
